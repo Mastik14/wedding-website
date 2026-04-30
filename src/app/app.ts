d73d1fcb4f7fcdaa0b7020d8ec8ad6573018817d
mastik14 <www.sergeypygach@mail.ru>
2026-05-01 00:54:32 +0300

    added venue component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { DateInvitation } from "./components/date-invitation/date-invitation";
+import { Venue } from "./components/venue/venue";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, DateInvitation],
+  imports: [Header, DateInvitation, Venue],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
