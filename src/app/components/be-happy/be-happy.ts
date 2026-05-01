4ff3c632478a5af6637ecf6b29a6f215fdd65a3e
mastik14 <www.sergeypygach@mail.ru>
2026-05-02 00:00:33 +0300

    added basic logic for images

--- src/app/components/be-happy/be-happy.ts
+++ src/app/components/be-happy/be-happy.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-be-happy',
   templateUrl: './be-happy.html',
   styleUrl: './be-happy.scss',
 })
-export class BeHappy {}
+export class BeHappy {
+  @ViewChild('happyBlock') happyBlock!: ElementRef;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          this.happyBlock.nativeElement.classList.add('active');
+        }
+      },
+      { threshold: 0.4 },
+    );
+
+    observer.observe(this.happyBlock.nativeElement);
+  }
+}
