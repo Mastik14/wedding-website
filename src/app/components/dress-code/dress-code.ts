e54f5a618fa1ff71b5120d5f3815e50e91a5f740
mastik14 <www.sergeypygach@mail.ru>
2026-05-01 21:54:41 +0300

    added position for image

--- src/app/components/dress-code/dress-code.ts
+++ src/app/components/dress-code/dress-code.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dress-code',
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './dress-code.html',
   styleUrl: './dress-code.scss',
 })
-export class DressCode {}
+export class DressCode {
+  @ViewChild('dressBlock') dressBlock!: ElementRef;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          this.dressBlock.nativeElement.classList.add('active');
+        }
+      },
+      { threshold: 0.3 },
+    );
+
+    observer.observe(this.dressBlock.nativeElement);
+  }
+}
