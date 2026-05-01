2b1d03870a14ec3a0290effb8d124dcb189a79e5
mastik14 <www.sergeypygach@mail.ru>
2026-05-01 18:13:55 +0300

    added images for dress code

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './components/header/header';
 import { DateInvitation } from "./components/date-invitation/date-invitation";
 import { Venue } from "./components/venue/venue";
 import { TimingOfTheDay } from "./components/timing-of-the-day/timing-of-the-day";
+import { DressCode } from "./components/dress-code/dress-code";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, DateInvitation, Venue, TimingOfTheDay],
+  imports: [Header, DateInvitation, Venue, TimingOfTheDay, DressCode],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/dress-code/dress-code.ts
+++ src/app/components/dress-code/dress-code.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './dress-code.html',
   styleUrl: './dress-code.scss',
 })
-export class DressCode {
-
-}
+export class DressCode {}
