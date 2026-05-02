cb8efbffba93882c2bf388967a8741440c7a7742
mastik14 <www.sergeypygach@mail.ru>
2026-05-02 18:27:14 +0300

    added fix for footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
+  private targetDate = new Date('2026-08-08T00:00:00');
 
+  private now = signal(Date.now());
+
+  constructor() {
+    setInterval(
+      () => {
+        this.now.set(Date.now());
+      },
+      60 * 60 * 1000,
+    );
+  }
+
+  public daysLeft = computed(() => {
+    const diff = this.targetDate.getTime() - this.now();
+
+    return Math.max(0, Math.ceil(diff / (1000 * 60 * 60 * 24)));
+  });
 }
