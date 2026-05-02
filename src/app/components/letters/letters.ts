c19435fe62a8d3bb6ba3a847707ccc6ef3accccb
mastik14 <www.sergeypygach@mail.ru>
2026-05-02 22:47:23 +0300

    added animation for letters

--- src/app/components/letters/letters.ts
+++ src/app/components/letters/letters.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-letters',
@@ -7,5 +7,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Letters {
+  @ViewChild('lettersBlock') lettersBlock!: ElementRef;
 
+ngAfterViewInit() {
+  const el = this.lettersBlock.nativeElement;
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        el.classList.add('active');
+      }
+    },
+    { threshold: 0.25 }
+  );
+
+  observer.observe(el);
+}
 }
