a20f5d326d82bde2abf32faf9bb03858eeceb554
mastik14 <www.sergeypygach@mail.ru>
2026-05-19 23:52:44 +0300

    added animation for skyes

--- src/app/components/timing-of-the-day/timing-of-the-day.ts
+++ src/app/components/timing-of-the-day/timing-of-the-day.ts
@@ -1,11 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-timing-of-the-day',
   templateUrl: './timing-of-the-day.html',
   styleUrl: './timing-of-the-day.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
 })
-export class TimingOfTheDay {
+export class TimingOfTheDay implements AfterViewInit {
+  @ViewChild('skyImage') skyImage!: ElementRef;
 
+  public ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      },
+      {
+        threshold: 0.35,
+      },
+    );
+
+    observer.observe(this.skyImage.nativeElement);
+  }
 }
