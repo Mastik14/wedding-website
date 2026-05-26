16734b2a80ee1c46822257f36c746108e7ec3b20
mastik14 <www.sergeypygach@mail.ru>
2026-05-26 22:22:24 +0300

    change text and fix styles

--- src/app/components/letters/letters.ts
+++ src/app/components/letters/letters.ts
@@ -1,26 +1,32 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-letters',
   templateUrl: './letters.html',
   styleUrl: './letters.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Letters {
+export class Letters implements AfterViewInit {
   @ViewChild('lettersBlock') lettersBlock!: ElementRef;
 
-ngAfterViewInit() {
-  const el = this.lettersBlock.nativeElement;
+  public ngAfterViewInit() {
+    const el = this.lettersBlock.nativeElement;
 
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.isIntersecting) {
-        el.classList.add('active');
-      }
-    },
-    { threshold: 0.25 }
-  );
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          el.classList.add('active');
+        }
+      },
+      { threshold: 0.25 },
+    );
 
-  observer.observe(el);
-}
+    observer.observe(el);
+  }
 }
